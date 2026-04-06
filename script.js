419b4da40201eccb85712f50e731114d84aa7f96
RoxtarKidd <132388317+RoxtarKidd@users.noreply.github.com>
2026-04-06 21:12:32 +0000

    Day One

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// add javascript here
+var playerName = prompt("What is your name?")
+
+document.getElementById("playBtn").addEventListener("click", function() {
+    document.getElementById("playBtn").disabled = true;
+    var difficulty = document.querySelector('input[name="level"]:checked').value;
+    var range = parseInt(difficulty);
+    var ranNum = Math.floor(Math.random() * range) + 1;
+    document.getElementById("msg").textContent = "Guess the number!";
+    document.getElementById("guessBtn").disabled = false;
+})
