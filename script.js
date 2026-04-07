a92e49ceae80dcbd0b9415805123fe42582b9a05
RoxtarKidd <132388317+RoxtarKidd@users.noreply.github.com>
2026-04-07 21:09:44 +0000

    Added Give Up Button

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var playerName = prompt("What is your name?");
 playerName = playerName.charAt(0).toUpperCase() + playerName.slice(1).toLowerCase();
-var difficulty = document.querySelector('input[name="level"]:checked').value;
-var range = parseInt(difficulty);
-var answer = Math.floor(Math.random() * range) + 1;
+var difficulty = null;
+var range = null;
+var answer = null;
 const tempArray = ["cold", "warm", "hot"];
 var temp = null;
 var wins = 0;
@@ -10,6 +10,11 @@ document.getElementById("playBtn").addEventListener("click", function() {
     document.getElementById("msg").textContent = "Guess the number!";
     document.getElementById("playBtn").disabled = true;
     document.getElementById("guessBtn").disabled = false;
+    document.getElementById("giveUpBtn").disabled = false;
+
+    difficulty = document.querySelector('input[name="level"]:checked').value;
+    range = parseInt(difficulty);
+    answer = Math.floor(Math.random() * range) + 1;;
 })
 
 
@@ -40,10 +45,17 @@ document.getElementById("guessBtn").addEventListener("click", function() {
     }
     else if (guess === answer) {
         document.getElementById("msg").textContent = "That's correct! Congratulations, " + playerName + "!";
-        document.getElementById("guessBtn").disabled = false;
         wins++;
         document.getElementById("wins").textContent = "Total Wins: " + wins;
         document.getElementById("playBtn").disabled = false;
         document.getElementById("guessBtn").disabled = true;
+        document.getElementById("giveUpBtn").disabled = true;
     }
 })
+
+document.getElementById("giveUpBtn").addEventListener("click", function() {
+    document.getElementById("msg").textContent = "The number was " + answer + ".";
+    document.getElementById("guessBtn").disabled = true;
+    document.getElementById("playBtn").disabled = false;
+    document.getElementById("giveUpBtn").disabled = true;
+});
